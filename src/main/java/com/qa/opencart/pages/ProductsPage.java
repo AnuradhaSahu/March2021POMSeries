e0e37491d77947bd20b052f1b1874eff276dfa4f
Anuradha <anugiit1992@gmail.com>
2021-06-28 15:59:39 +0530

    updated product page

--- src/main/java/com/qa/opencart/pages/ProductsPage.java
+++ src/main/java/com/qa/opencart/pages/ProductsPage.java
@@ -1,5 +1,17 @@
 package com.qa.opencart.pages;
 
+import org.openqa.selenium.WebDriver;
+
+import com.qa.opencart.utils.ElementUtil;
+
 public class ProductsPage {
+	private WebDriver driver;
+	private ElementUtil elementUtil;
+
+	
+	public ProductsPage() {
+		this.driver=driver;
+		elementUtil=new ElementUtil(this.driver);
+	}
 
 }
